1645f66e0b49c15d6f1727917d91753a5dbf42dc
zjq <kevin198528@163.com>
2018-06-14 17:10:14 +0800

    add

--- sc_proto/sc_proto.c
+++ sc_proto/sc_proto.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "./sc_proto.h"
 
-#define with_init(code) {\
+#define DEBUG 1
+
+#define with_init(code) \
+do{\
 	Py_Initialize();\
 	get_current();\
-	{code}\
+	do{code}while(0);\
 	Py_Finalize();\
-}
+}while(0);
 
 #define __py_init__ {\
 Py_Initialize();\
@@ -17,23 +20,39 @@ get_current();\
 Py_Finalize();\
 }
 
+#ifdef DEBUG
+#define __debug__(code) \
+do{\
+	printf("\ndebug in - %s : %s\n", __FILE__, __func__);\
+	do{code}while(0);\
+	printf("debug out - %s : %s\n\n", __FILE__, __func__);\
+}while(0);
+#else
+#define __debug__(code) \
+do{\
+	do{code}while(0);\
+}while(0);
+#endif
+	
 dev_list g_dev_list;
 
-const char * s_py_path = "sys.path.append('../../PycharmProjects/\
-server_client/server_client/devices')";
-
 void get_current(void)
-{
+{ 
    PyRun_SimpleString("import sys");
    PyRun_SimpleString("sys.path.append('./')");
-   PyRun_SimpleString(s_py_path);
+	PyRun_SimpleString("sys.path.append('/home/zjq/PycharmProjects/\
+server_client/server_client/')");
+//	PyRun_SimpleString();
 }
 
 PyObject * get_func_from_module(const char * m, const char * f)
 {
    PyObject *pModule = NULL, *pDict = NULL, *pFunc = NULL;
+
+	__debug__(
    pModule = PyImport_ImportModule(m);
    pFunc = PyObject_GetAttrString(pModule, f);
+	)
    return pFunc;
 }
 
@@ -45,11 +64,11 @@ p_scanner_handle sc_scanner_create(void)
 {
 	PyObject *p_class = NULL, *p_ins = NULL;
 
+	__debug__(	
 	__py_init__
-
-   p_class = get_func_from_module("scanner", "Scanner");
+   p_class = get_func_from_module("devices.scanner", "Scanner");
 	p_ins = PyObject_CallObject(p_class, NULL);
-
+	)
 	return (p_scanner_handle)p_ins;
 }
 
@@ -57,19 +76,27 @@ void sc_scanner_delete(p_scanner_handle p_scanner)
 {
 	PyObject *pFunc = NULL;
 
-	if (p_scanner) {
+	__debug__(
+	if (p_scanner) 
 		Py_DECREF(p_scanner);	
-		printf("free p_scanner\n");
-	}
-
+		
 	__py_release__
+	)
 }
 
 p_dev_list sc_scanner_scan(p_scanner_handle p_scanner)
 {
-	PyObject *p_ins = (PyObject *)p_scanner;
+	PyObject *p_list = NULL, *p_str = NULL;	
+	char * p_name = NULL;
 
-	printf("in sc_scanner_scan : 0x%x\n", p_ins);
-	PyObject_CallMethod(p_ins, "scanning", NULL);
+	__debug__(
+	PyObject *p_ins = (PyObject *)p_scanner;
+	p_list = PyObject_CallMethod(p_ins, "scanning", NULL);
+	for (int index = 0; index < PyList_Size(p_list); index++) {
+		p_str = PyList_GetItem(p_list, index);
+		PyArg_Parse(p_str, "s", &p_name);
+		printf("device_name : %s\n", p_name);
+	}
+	)
 }
 
